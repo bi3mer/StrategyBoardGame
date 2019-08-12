fc912a7cbc42d85b5c9f8cc0f5f2deafe179beee
Colan Biemer <colan@comcast.net>
2019-08-11 22:27:25 -0500

    got the states looking pretty solid for the menu. Feeling #good

--- Assets/Scripts/State/MenuState.cs
+++ Assets/Scripts/State/MenuState.cs
@@ -9,21 +9,32 @@ public class MenuState : MonoState
     public class StateImplementation : TriggeringState<StateTrigger>
     {
         private readonly GameObject menuObject;
+        private readonly Button startGameButton;
 
-        public StateImplementation(GameObject menuObject)
+        public StateImplementation(GameObject menuObject, Button startGameButton)
         {
+            Assert.IsNotNull(startGameButton);
             Assert.IsNotNull(menuObject);
+
+            this.startGameButton = startGameButton;
             this.menuObject = menuObject;
         }
 
         protected override void OnStateEnter()
         {
+            startGameButton.onClick.AddListener(OnStartGameButtonClick);
             menuObject.SetActive(true);
         }
 
         protected override void OnStateExit()
         {
+            startGameButton.onClick.RemoveListener(OnStartGameButtonClick);
             menuObject.SetActive(false);
+        }
+
+        private void OnStartGameButtonClick()
+        {
+            Debug.Log("oh hai world");
         }
     }
 
@@ -32,7 +43,6 @@ public class MenuState : MonoState
 
     protected override void Awake()
     {
-        Assert.IsNotNull(startGameButton);
-        State = new StateImplementation(gameObject);
+        State = new StateImplementation(gameObject, startGameButton);
     }
 }
